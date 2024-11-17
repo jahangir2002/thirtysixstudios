f0eea6d30a603a4f95b4df2015cdcab3064e2577
jahangir2002 <jahangirshaikh553@gmail.com>
2024-11-17 16:09:23 +0530

    thirtysixstudio Redesign project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+import Canvas from "./canvas"
+
+function App() {
+  return <>
+  <div className="w-full h-screen bg-black text-white">
+    <Canvas/>
+  </div>
+  </>
+}
+
+export default App

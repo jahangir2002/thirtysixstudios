2644e1ee144d00e40eb58b0e00031a580b3f63f4
jahangir2002 <jahangirshaikh553@gmail.com>
2024-11-19 16:10:15 +0530

    add some sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   }, []);
   return (<>
     <div className="relative w-full h-screen">
-      {/* {data[0].map((canvasdets, canvasIndex) => (
+      {data[0].map((canvasdets, canvasIndex) => (
         <Canvas key={canvasIndex} details={canvasdets} />
-      ))} */}
-      <div className="w-full h-screen text-white">
+      ))}
+      <div className="w-full h-screen relative z-[1] text-white">
         <nav className=" w-full flex justify-between items-center px-10 py-6 z-50">
           <div className="logo">
             <h2 className="text-white text-2xl font-bold">Thirtysixstudio</h2>
@@ -30,21 +30,25 @@ function App() {
             ))}
           </div>
         </nav>
-        <div className="textcontainer px-[25%] w-full">
-          <div className="text w-[40%]">
+        <div className="textcontainer px-[17%] mt-20 w-full">
+          <div className="text w-[60%]">
             <h3 className="text-3xl leading-[1.3] " >At Thirtysixstudio, we build immersive digital experiences for brands with a purpose.</h3>
             <p className="text-sm mt-10 font-normal">We’re a boutique production studio focused on design, motion, and creative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.</p>
             <p className="text-md mt-10">Scroll</p>
           </div>
         </div>
 
-        <div className="w-full absolute text-center mt-40">
+        <div className="w-full text-center mt-40">
           <h1 className="text-white text-[12rem] font-normal leading-[none] ">Thirtysixstudio</h1>
         </div>
 
       </div>
-      
+
     </div>
+
+   
+
+
 
 
 
